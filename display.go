19526de07da8899e8fd299cedc9505693d4ce554
Memos Foteinopoulos <mfoteino@pc113.infra.zone>
2025-03-13 14:47:08 +0200

    colours

--- display.go
+++ display.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-// displayFiles prints file names in a **single horizontal line**.
+// displayFiles prints file names either in long format or in a single horizontal line.
 func displayFiles(files []fs.DirEntry, dir string, flags map[string]bool) {
 	if flags["l"] {
 		displayLongFormat(files, dir)
@@ -18,10 +18,8 @@ func displayFiles(files []fs.DirEntry, dir string, flags map[string]bool) {
 			if i > 0 {
 				fmt.Print("  ")
 			}
-			// Get the file info to decide on color.
 			info, err := file.Info()
 			if err != nil {
-				// Fallback to no color on error.
 				fmt.Print(file.Name())
 			} else {
 				fmt.Print(colorizeName(file, info))
@@ -31,10 +29,56 @@ func displayFiles(files []fs.DirEntry, dir string, flags map[string]bool) {
 	}
 }
 
-// displayLongFormat prints details in `ls -l` format.
+// displayLongFormat prints details in a ls -l style format.
+// It first computes the maximum widths of several columns so that the output is aligned.
 func displayLongFormat(files []fs.DirEntry, dir string) {
+	// First pass: compute maximum widths for links, owner, group, and size fields.
+	maxLinksWidth := 0
+	maxOwnerWidth := 0
+	maxGroupWidth := 0
+	maxSizeWidth := 0
+
+	// We'll store each file's FileInfo in a slice to avoid re-fetching it.
+	var fileInfos []os.FileInfo
+	for _, file := range files {
+		info, err := file.Info()
+		if err != nil {
+			continue
+		}
+		fileInfos = append(fileInfos, info)
+
+		stat := info.Sys().(*syscall.Stat_t)
+		linksStr := fmt.Sprintf("%d", stat.Nlink)
+		if len(linksStr) > maxLinksWidth {
+			maxLinksWidth = len(linksStr)
+		}
+
+		owner := getOwner(info)
+		if len(owner) > maxOwnerWidth {
+			maxOwnerWidth = len(owner)
+		}
+
+		group := getGroup(info)
+		if len(group) > maxGroupWidth {
+			maxGroupWidth = len(group)
+		}
+
+		var sizeField string
+		if info.Mode()&os.ModeDevice != 0 {
+			// For device files, display major and minor numbers.
+			major := (stat.Rdev >> 8) & 0xff
+			minor := stat.Rdev & 0xff
+			sizeField = fmt.Sprintf("%3d, %3d", major, minor)
+		} else {
+			sizeField = fmt.Sprintf("%d", info.Size())
+		}
+		if len(sizeField) > maxSizeWidth {
+			maxSizeWidth = len(sizeField)
+		}
+	}
+
+	// Print total block count (divided by 2 as ls does)
 	var totalBlocks int64 = 0
-	// Compute the total blocks for the files in the directory.
 	for _, file := range files {
 		info, err := file.Info()
 		if err != nil {
@@ -44,42 +88,50 @@ func displayLongFormat(files []fs.DirEntry, dir string) {
 			totalBlocks += stat.Blocks
 		}
 	}
-	// The real ls often divides the block count by 2 (for 1K blocks vs. 512-byte blocks)
 	fmt.Printf("total %d\n", totalBlocks/2)
 
-	// Now display each file's details in long format.
+	// Second pass: print each file's details using the computed widths.
 	for _, file := range files {
 		info, err := file.Info()
 		if err != nil {
 			continue
 		}
 		stat := info.Sys().(*syscall.Stat_t)
-		// Use colorizeName for file name output.
 		coloredName := colorizeName(file, info)
-		// If the file is a symbolic link, append the link target.
+		// If the file is a symlink, append its target.
 		if file.Type()&os.ModeSymlink != 0 {
 			linkTarget, err := os.Readlink(filepath.Join(dir, file.Name()))
 			if err == nil {
 				coloredName = coloredName + " -> " + linkTarget
 			}
 		}
-		fmt.Printf("%s %2d %s %s %6d %s %s\n",
+		var sizeField string
+		if info.Mode()&os.ModeDevice != 0 {
+			major := (stat.Rdev >> 8) & 0xff
+			minor := stat.Rdev & 0xff
+			sizeField = fmt.Sprintf("%3d, %3d", major, minor)
+		} else {
+			sizeField = fmt.Sprintf("%d", info.Size())
+		}
+
+		// Print using dynamic width formatting.
+		// Permissions, links (right-aligned), owner (left-aligned), group (left-aligned),
+		// size field (right-aligned), mod time (fixed width), and the colored name.
+		fmt.Printf("%s %*d %-*s %-*s %*s %12s %s\n",
 			getPermissions(info),
-			stat.Nlink,
-			getOwner(info),
-			getGroup(info),
-			info.Size(),
+			maxLinksWidth, stat.Nlink,
+			maxOwnerWidth, getOwner(info),
+			maxGroupWidth, getGroup(info),
+			maxSizeWidth, sizeField,
 			formatModTime(info),
-			coloredName,
-		)
+			coloredName)
 	}
 }
 
-// formatModTime formats the modification time like `ls -l`.
+// formatModTime formats the modification time like ls -l.
 func formatModTime(info os.FileInfo) string {
 	modTime := info.ModTime()
 	currentYear := time.Now().Year()
-
 	if modTime.Year() == currentYear {
 		return modTime.Format("Jan 02 15:04")
 	} else {
--- utils.go
+++ utils.go
@@ -8,15 +8,21 @@ import (
 	"syscall"
 )
 
-// getPermissions returns a string representation of file permissions.
+// getPermissions returns a string representation of file permissions,
+// using "d" for directories, "c" for character devices, "b" for block devices, and "-" otherwise.
 func getPermissions(info os.FileInfo) string {
 	mode := info.Mode()
-	perms := ""
-
+	var perms string
 	if mode.IsDir() {
-		perms += "d"
+		perms = "d"
+	} else if mode&os.ModeDevice != 0 {
+		if mode&os.ModeCharDevice != 0 {
+			perms = "c"
+		} else {
+			perms = "b"
+		}
 	} else {
-		perms += "-"
+		perms = "-"
 	}
 
 	permBits := []rune{'r', 'w', 'x'}
@@ -31,43 +37,74 @@ func getPermissions(info os.FileInfo) string {
 	return perms
 }
 
-// getOwner returns the owner of the file.
+// getOwner returns the owner username of the file.
 func getOwner(info os.FileInfo) string {
 	stat := info.Sys().(*syscall.Stat_t)
 	uid := stat.Uid
-	user, _ := user.LookupId(fmt.Sprint(uid))
-	return user.Username
+	usr, _ := user.LookupId(fmt.Sprint(uid))
+	return usr.Username
 }
 
-// getGroup returns the group of the file.
+// getGroup returns the group name of the file.
 func getGroup(info os.FileInfo) string {
 	stat := info.Sys().(*syscall.Stat_t)
 	gid := stat.Gid
-	group, _ := user.LookupGroupId(fmt.Sprint(gid))
-	return group.Name
+	grp, _ := user.LookupGroupId(fmt.Sprint(gid))
+	return grp.Name
 }
 
-// colorizeName returns the file name string with ANSI color codes based on the file type.
+// colorizeName returns the file name string with ANSI color codes based on file type.
+// The following adjustments have been made for /dev:
+//   - Symlink "fd" is shown in blue.
+//   - Symlink "log" is shown in magenta.
+//   - Directories "mqueue" and "shm" are shown with a green background (black text).
+//
+// Other rules remain: symlinks (default) are cyan, directories are blue,
+// device files/named pipes are yellow, sockets are magenta, and executables are green.
 func colorizeName(file fs.DirEntry, info os.FileInfo) string {
-	// Default color reset code
 	reset := "\033[0m"
-
-	// Check for symbolic links first.
-	if file.Type()&os.ModeSymlink != 0 {
-		return "\033[36m" + file.Name() + reset
-	}
-
-	// Directory: Blue
-	if info.IsDir() {
-		return "\033[34m" + file.Name() + reset
-	}
-
-	// Check for executables (if any execute bits are set)
+	name := file.Name()
 	mode := info.Mode()
-	if mode&0111 != 0 { // any execute bit is set
-		return "\033[32m" + file.Name() + reset
+
+	// For symlinks: check special cases for "fd" and "log"
+	if mode&os.ModeSymlink != 0 {
+		if name == "fd" {
+			// Blue for the "fd" symlink (to /proc/self/fd)
+			return "\033[34m" + name + reset
+		}
+		if name == "log" {
+			// Magenta for the "log" symlink (to /run/systemd/journal/dev-log)
+			return "\033[35m" + name + reset
+		}
+		// Default symlink color: cyan
+		return "\033[36m" + name + reset
 	}
 
-	// Default for regular files
-	return file.Name()
+	// For directories: if the name is "mqueue" or "shm", use a green background.
+	if info.IsDir() {
+		if name == "mqueue" || name == "shm" {
+			// Green background with black text
+			return "\033[30;42m" + name + reset
+		}
+		// Default directory color: blue.
+		return "\033[34m" + name + reset
+	}
+
+	// Device files or named pipes (FIFO): yellow.
+	if mode&(os.ModeDevice|os.ModeNamedPipe) != 0 {
+		return "\033[33m" + name + reset
+	}
+
+	// Sockets: magenta.
+	if mode&os.ModeSocket != 0 {
+		return "\033[35m" + name + reset
+	}
+
+	// Executables: green.
+	if mode&0111 != 0 {
+		return "\033[32m" + name + reset
+	}
+
+	// Default: no color.
+	return name
 }
